9e52a96a8cd53b91eddb25cc7c8c9dbc549b044f
BrendanArens <arensBrendan@gmail.com>
2022-10-19 16:32:32 -0500

    error handling

--- src/main/java/com/example/commerce/controller/LocationController.java
+++ src/main/java/com/example/commerce/controller/LocationController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.stream.Collectors;
 
@@ -19,27 +20,48 @@ public class LocationController {
 
     @GetMapping("/location")
     public ResponseEntity<?> getLocations() {
-        return new ResponseEntity<>(locationService.getAll().stream().map(LocationDTO::new).collect(Collectors.toList()), HttpStatus.OK);
+        try {
+            return new ResponseEntity<>(locationService.getAll().stream().map(LocationDTO::new).collect(Collectors.toList()), HttpStatus.OK);
+        } catch (ResponseStatusException rse) {
+            return new ResponseEntity<>(rse.getMessage(), rse.getStatus());
+        }
     }
 
     @GetMapping("/location/{locId}")
     public ResponseEntity<?> getLocationsById(@PathVariable(value = "locId") Long locId) {
-        return new ResponseEntity<>(new LocationDTO(locationService.getById(locId)), HttpStatus.OK);
+        try {
+            return new ResponseEntity<>(new LocationDTO(locationService.getById(locId)), HttpStatus.OK);
+        } catch (ResponseStatusException rse) {
+            return new ResponseEntity<>(rse.getMessage(), rse.getStatus());
+        }
     }
 
     @PostMapping("/location")
     public ResponseEntity<?> createLocation(@RequestBody Location location) {
-        return new ResponseEntity<>(new LocationDTO(locationService.create(location)), HttpStatus.CREATED);
+        try {
+            return new ResponseEntity<>(new LocationDTO(locationService.create(location)), HttpStatus.CREATED);
+        } catch (ResponseStatusException rse) {
+            return new ResponseEntity<>(rse.getMessage(), rse.getStatus());
+        }
     }
 
     @PutMapping("/location/{locId}")
     public ResponseEntity<?> updateLocation(@PathVariable(value = "locId") Long locId, @RequestBody Location location) {
-        return new ResponseEntity<>(new LocationDTO(locationService.update(locId, location)), HttpStatus.ACCEPTED);
+        try {
+            return new ResponseEntity<>(new LocationDTO(locationService.update(locId, location)), HttpStatus.ACCEPTED);
+        } catch (ResponseStatusException rse) {
+            return new ResponseEntity<>(rse.getMessage(), rse.getStatus());
+        }
     }
 
     @DeleteMapping("/location/{locId}")
     public ResponseEntity<?> deleteLocation(@PathVariable(value = "locId") Long locId) {
-        locationService.delete(locId);
-        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+        try {
+            locationService.delete(locId);
+            return new ResponseEntity<>(HttpStatus.ACCEPTED);
+        } catch (
+                ResponseStatusException rse) {
+            return new ResponseEntity<>(rse.getMessage(), rse.getStatus());
+        }
     }
 }
