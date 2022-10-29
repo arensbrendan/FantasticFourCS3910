c1fedb0596de58da6fc0cff6bf11acd1d5b200e4
Jackson Gordon <jag04460@ucmo.edu>
2022-10-29 17:04:01 -0500

    MEGAN COMMIT: Updated Time.js and Time.css includes progress bar, calendar, and continue button

--- bookfrontend/src/pages/Time.js
+++ bookfrontend/src/pages/Time.js
@@ -1,11 +1,80 @@
+import '../components/Header';
+import './Time.css';
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
 
-//IF YOU'RE SEEING AN ERROR WHEN CLICKING THE "TIME" PAGE, THAT IS ON PURPOSE
+//Function to build the calendar.
+function BuildCalendar(){
+    const [value, onChange] = useState(new Date());
+
+    return (
+        <div>
+            <Calendar onChange={onChange} value={value} calendarType={"US"} minDate={new Date()}
+                /* Needs implementation: click on Nov 4th to show times (11:05 am)> */ />
+        </div>
+    );
+}
+
 export default function Time(){
 
     return(
         <>
+            {/*Spaces to get under the header*/}
+            <div>Header Hides</div>
+            <div>Header Hides</div>
+            <div>Header Hides</div>
+            <div>Header Hides</div>
 
+            <prog>
+                {/*progbarfix is a fix for every prog bar bedsides the one on the purpose page, this is due to a mistake in the CSS in the purpose.css*/}
+                <div className={"progbarfix"}>
+                    {/*Start Navbar*/}
+                    {/*each li is what you can consider to be a circle, with the label li following*/}
+                    <div className="container">
+                        <ul className="progress-bar">
+                            {/*DO NOT KNOW HOW TO DEAL WITH GOING TO A FUTURE PAGE PAST THE ONE IN FRONT IF CUSTOMER HAS BEEN TO IT, MAY SCRAP FEATURE*/}
+                            <li>
+                                {/*First status circle*/}
+                                <div className = "done"><a  href={"./"}><b>1</b></a></div>
+                                <div><a  href={"./"}><b>Purpose</b></a></div>
+                            </li>
+                            <li>
+                                {/*Second status circle*/}
+                                <div className = "done"><a href = "./Location"><b>2</b></a></div>
+                                <div><a href = "./Location"><b>Location</b></a></div>
+                            </li>
+                            <li>
+                                {/*Third status circle*/}
+                                <div className = {"current"}><a href = "./Time" ><b>3</b></a></div>
+                                <div><a href = "./Time"><b>Time</b></a></div>
+                            </li>
+                            <li>
+                                {/*Fourth status circle*/}
+                                <div className = "./Details"><a href = "./Details"><b>4</b></a></div>
+                                <div><a href = "./Details"><b>Details</b></a></div>
+                            </li>
+                            <li>
+                                {/*Fifth status circle*/}
+                                <div><a href = "./Review"><b>5</b></a></div>
+                                <div><a href = "./Review"><b>Review</b></a></div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </prog>
+
+            {/*heading and subheading*/}
+            <h1>What time works best for you?</h1>
+            <p className={"subheading"}>Choose a date from the calender and an available time from the options below.</p>
+
+            {/*Renders the calendar.*/}
+            <BuildCalendar />
+
+            {/*button used to continue to next page*/}
+            <continue>
+                <a href= "./Details" className={"buttonP"}><button>Continue</button></a>
+            </continue>
         </>
     )
-
 }
+
